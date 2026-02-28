71800644be958bec363e3821931356ee8ed68d5c
praniil <pparajuli15@gmail.com>
2026-02-28 22:11:59 +0545

    feat: compute principal components

--- src/standardize_dataset.py
+++ src/standardize_dataset.py
@@ -1,18 +1,21 @@
 import pandas as pd 
 import numpy as np 
-import torch.nn as nn
-
-iris_dataset = pd.read_csv('../dataset/IRIS.csv')
-print(len(iris_dataset))
-
-iris_features = iris_dataset.drop(columns=['species']) 
-
-#standardixze dataset
-iris_features = (iris_features - iris_features.mean()) / iris_features.std()
-
-#convert dataframe to numpy array
-iris_features = iris_features.numpy()
-
-print(iris_features)
 
 
+def standardize_dataset():
+    iris_dataset = pd.read_csv('../dataset/IRIS.csv')
+    print(len(iris_dataset))
+
+    iris_features = iris_dataset.drop(columns=['species']) 
+
+    #standardixze dataset
+    iris_features = (iris_features - iris_features.mean()) / iris_features.std()
+
+    #convert dataframe to numpy array
+    iris_features = iris_features.to_numpy()
+
+    return iris_features, len(iris_features)
+
+
+if __name__ == "__main__":
+    standardize_dataset()
